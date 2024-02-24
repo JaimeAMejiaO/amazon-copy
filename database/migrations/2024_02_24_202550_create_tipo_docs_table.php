09c60ed3dfff0905f619fe356a2f374d7776de44
Jaime Mejia <jaime.mejia@utp.edu.co>
2024-02-24 15:39:02 -0500

    migrations

--- database/migrations/2024_02_24_202550_create_tipo_docs_table.php
+++ database/migrations/2024_02_24_202550_create_tipo_docs_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tipo_docs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->timestamps();
+            $table->string('acronimo_tipo', 20)->default('-');
+            $table->string('nombre_tipo', 50);
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tipo_docs');
     }
 };
---
+++ database/migrations/2024_02_24_203015_create_producto_detalles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto_detalles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('id_prod');
+            $table->integer('precio');
+            $table->integer('cant');
+
+            $table->foreign('id_prod')->references('id')->on('productos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto_detalles');
+    }
+};
