1d1486f39701a6d8091312a465ddda2d7135d405
Jaime Mejia <jaime.mejia@utp.edu.co>
2024-03-03 19:54:32 -0500

    Change: Lenguaje del proyecto Change: Registro y logeo

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,17 +31,19 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'nombre' => ['required', 'regex:/^[\pL\s]+$/u', 'max:50'],
+            'apell' => ['required', 'regex:/^[\pL\s]+$/u', 'max:50'],
+            'email' => ['required', 'email', 'max:100', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'nombre' => $request->nombre,
+            'apell' => $request->apell,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+        
         event(new Registered($user));
 
         Auth::login($user);
