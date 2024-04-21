64f8ff46a24952d870cdc29587867d5c80cdae73
DAZAREAPER\USUARIO <curiousjet991@gmail.com>
2024-04-21 16:38:23 -0500

    change: cambios visuales marcas

--- resources/views/livewire/marcas.blade.php
+++ resources/views/livewire/marcas.blade.php
@@ -1,20 +1,38 @@
-<div>
-    @if($marcas->count() > 0)
-        <ul>
-            @foreach($marcas as $marca)
-                <li>
-                    {{ $marca->nombre }}
-                    <button><i class="fa fa-pencil"></i></button>
-                    <button wire:click="delete({{$marca->id}})"><i class="fa fa-trash"></i></button>
-                </li>
-            @endforeach
-        </ul>
+<div class="container mt-4">
+    @if ($marcas->count() > 0)
+        <div class="card" style="margin-left:40%;margin-right:40% ">
+            <div class="card-body" style="">
+                <h5 class="card-title">Marcas existentes</h5>
+                <ul class="list-group list-group-flush">
+                    @foreach ($marcas as $marca)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            {{ $marca->nombre }}
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-sm btn-outline-primary" wire:click="editar({{ $marca->id }})">
+                                    <i class="fa fa-pencil"></i>
+                                </button>
+                                <button type="button" class="btn btn-sm btn-outline-danger" wire:click="delete({{ $marca->id }})" onclick="return confirm('¿Está seguro que desea eliminar esta marca?')">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     @else
-        <p>No hay marcas creadas.</p>
+        <div class="alert alert-info mt-4" role="alert">
+            No hay marcas creadas.
+        </div>
     @endif
 
-    <div>
-        <input wire:model.blur="nombre_marca" id="nombre_marca" type="text" name="nombre_marca" placeholder="Nombre de la marca">
-        <button wire:click="store">Guardar</button>
+    <div class="card mt-4" style="margin-left:40%;margin-right:40% ">
+        <div class="card-body" >
+            <h5 class="card-title">Agregar nueva marca</h5>
+            <div class="input-group mb-3">
+                <input wire:model.defer="nombre_marca" type="text" class="form-control" placeholder="Nombre de la marca" aria-label="Nombre de la marca" aria-describedby="button-addon2">
+                <button class="btn btn-primary" type="button" wire:click="store" id="button-addon2">Guardar</button>
+            </div>
+        </div>
     </div>
 </div>
