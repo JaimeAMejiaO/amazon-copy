a5e07e6225fbfcf66a0a92aaa5741233688af366
JaimeAMejiaO <jaime.mejia@utp.edu.co>
2024-05-22 22:00:45 -0500

    Change: INtento por subir imagenes #5

--- config/app.php
+++ config/app.php
@@ -55,6 +55,7 @@ return [
     */
 
     'url' => env('APP_URL', 'https://walrus-app-4b6ny.ondigitalocean.app/'),
+    //'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
