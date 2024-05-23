964428a95e3746f15930cd72230c0a82e97081bf
JaimeAMejiaO <jaime.mejia@utp.edu.co>
2024-05-22 21:41:30 -0500

    Change: Try upload images in web #4

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://walrus-app-4b6ny.ondigitalocean.app/'),
 
     'asset_url' => env('ASSET_URL', '/'),
 
